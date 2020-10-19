e2befeb32b016b115f1c4e1f1906c5cc81daa1e0
Brandon Slinkard <brandon.slinkard@nelnet.net>
2020-10-19 15:36:52 -0500

    Add placeholder for tq formula

---
+++ Formula/tq.rb
@@ -0,0 +1,11 @@
+class Tq < Formula
+  desc "Command line TOML Processor"
+  homepage "https://github.com/4rbor/tq"
+  url ""
+  sha256 ""
+  version ""
+
+  def install
+    bin.install "tq"
+  end
+end
